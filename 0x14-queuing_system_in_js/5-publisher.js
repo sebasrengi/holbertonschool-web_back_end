3a4b0ceff28ea766110afdd0ec3fd39761d36b00
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2022-12-10 04:13:47 +0100

    Update 5-publisher.js

--- 0x14-queuing_system_in_js/5-publisher.js
+++ 0x14-queuing_system_in_js/5-publisher.js
@@ -7,12 +7,14 @@ client.on("error", (error) => {
       console.log('Redis client connected to the server');
   });
 
-client.subscribe("holberton school channel");
+const publishMessage = (message, time) => {
+  setTimeout(() => {
+    console.log(`About to send ${message}`);
+    client.publish('holberton school channel', message);
+  }, time);
+}
 
-client.on('message', (channel, message) => {
-  console.log(message);
-  if (message === 'KILL_SERVER') {
-    client.unsubscribe(channel);
-    process.exit(0);
-  }
-});
+publishMessage("Holberton Student #1 starts course", 100);
+publishMessage("Holberton Student #2 starts course", 200);
+publishMessage("KILL_SERVER", 300);
+publishMessage("Holberton Student #3 starts course", 400);

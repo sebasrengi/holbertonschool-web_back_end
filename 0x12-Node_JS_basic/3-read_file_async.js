2e5107913592bf0383988b6037b9eea842068302
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2023-02-05 13:39:16 -0500

    Update 3-read_file_async.js

--- 0x12-Node_JS_basic/3-read_file_async.js
+++ 0x12-Node_JS_basic/3-read_file_async.js
@@ -1,36 +1,35 @@
-const fs = require('fs');
+const fsp = require('fs').promises;
 
-function countStudents(path) {
-  const promise = (res, rej) => {
-    fs.readFile(path, 'utf8', (error, data) => {
-      if (error) rej(Error('Cannot load the database'));
-      const messages = [];
-      let message;
-      const content = data.toString().split('\n');
-      let students = content.filter((item) => item);
-      students = students.map((item) => item.split(','));
-      const nStudents = students.length ? students.length - 1 : 0;
-      message = `Number of students: ${nStudents}`;
-      console.log(message);
-      messages.push(message);
-      const subjects = {};
-      for (const i in students) {
-        if (i !== 0) {
-          if (!subjects[students[i][3]]) subjects[students[i][3]] = [];
-          subjects[students[i][3]].push(students[i][0]);
-        }
-      }
-      delete subjects.subject;
-      for (const key of Object.keys(subjects)) {
-        message = `Number of students in ${key}: ${
-          subjects[key].length
-        }. List: ${subjects[key].join(', ')}`;
-        console.log(message);
-        messages.push(message);
-      }
-      res(messages);
-    });
-  };
-  return new Promise(promise);
-}
+const countStudents = async (path) => {
+  try {
+    const promise = await fsp.readFile(path, 'utf8');
+
+    let lines = promise.split(/\r?\n/);
+    lines.shift();
+    lines = lines.filter((line) => line !== '');
+
+    console.log(`Number of students: ${lines.length}`);
+
+    const cs = lines
+      .filter((line) => line.endsWith('CS'))
+      .map((line) => {
+        const student = line.split(',');
+        return student[0];
+      });
+    console.log(`Number of students in CS: ${cs.length}. List: ${cs.join(', ')}`);
+
+    const swe = lines
+      .filter((line) => line.endsWith('SWE'))
+      .map((line) => {
+        const student = line.split(',');
+        return student[0];
+      });
+    console.log(`Number of students in SWE: ${swe.length}. List: ${swe.join(', ')}`);
+
+    return { lines, cs, swe };
+  } catch (err) {
+    throw Error('Cannot load the database');
+  }
+};
+
 module.exports = countStudents;

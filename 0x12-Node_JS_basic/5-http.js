ebbc51b1d1e2d8648e2292e7ce624496f2c3b6fa
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2023-02-05 13:41:19 -0500

    Update 5-http.js

--- 0x12-Node_JS_basic/5-http.js
+++ 0x12-Node_JS_basic/5-http.js
@@ -1,20 +1,32 @@
 const http = require('http');
 const countStudents = require('./3-read_file_async');
 
+const port = 1245;
+
 const app = http.createServer(async (req, res) => {
+  const { method, url } = req;
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  if (req.url === '/') res.write('Hello Holberton School!');
-  if (req.url === '/students') {
-    res.write('This is the list of our students\n');
-    try {
-      const data = await countStudents(process.argv[2]);
-      res.end(`${data.join('\n')}`);
-    } catch (error) {
-      res.end(error.message);
+  if (method === 'GET') {
+    if (url === '/') {
+      res.end('Hello Holberton School!');
+    } else if (url === '/students') {
+      await countStudents(process.argv[2])
+        .then((value) => {
+          res.write('This is the list of our students\n');
+          res.write(`Number of students: ${value.lines.length}\n`);
+          res.write(`Number of students in CS: ${value.cs.length}. List: ${value.cs.join(', ')}\n`);
+          res.write(`Number of students in SWE: ${value.swe.length}. List: ${value.swe.join(', ')}`);
+          res.end();
+        })
+        .catch((err) => {
+          res.write('This is the list of our students\n');
+          res.end(err.message);
+        });
     }
   }
-  res.end();
 });
-app.listen(1245);
+
+app.listen(port);
+
 module.exports = app;

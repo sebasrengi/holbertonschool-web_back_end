0cc1d6ff1818036c078df30bad67d2dbd0ee91bf
sebasrengi <85517658+sebasrengi@users.noreply.github.com>
2023-02-05 13:17:56 -0500

    Update api.js

--- 0x13-unittests_in_js/10-api/api.js
+++ 0x13-unittests_in_js/10-api/api.js
@@ -1,22 +1,47 @@
-'use strict';
-const express = require('express');
-const app = express();
-const port = 7865;
+const express = require('express')
+const app = express()
+const port = 7865
 
-app.get('/', (req, res) => res.end('Welcome to the payment system'))
-app.get('/cart/:id([0-9]+)', (req, res) => {
-  res.end(`Payment methods for cart ${req.params.id}`)
+app.use(express.json())
+
+app.get('/', (req, res) => {
+  res.status(200)
+  res.send('Welcome to the payment system')
 })
+
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params
+  console.log(parseInt(id))
+  if (isNaN(parseInt(id))) {
+    res.status(404)
+    res.send("Not a number")
+  } else {
+    res.status(200)
+    res.send(`Payment methods for cart ${id}`)
+  }
+})
+
 app.get('/available_payments', (req, res) => {
-  const obj = {
+  res.status(200)
+  res.send({
     payment_methods: {
       credit_cards: true,
-      paypal: false,
-    },
-  };
-  res.json(obj);
-});
+      paypal: false
+    }
+  })
+})
+
 app.post('/login', (req, res) => {
-  res.end(`Welcome ${req.body.userName}`);
-});
-app.listen(port, () => console.log(`API available on localhost port ${port}`));
+  const { userName } = req.body
+  if (userName) {
+    res.status(200)
+    res.send(`Welcome ${userName}`)
+  } else {
+    res.status(404)
+    res.end()
+  }
+})
+
+app.listen(port, () => {
+  console.log(`API available on localhost port 7865`)
+})
